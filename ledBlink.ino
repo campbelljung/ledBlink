b3e54c48ed3ae7da9904fd815ca51bdb3cbce734
campbelljung <cj1597@nyu.edu>
2023-11-14 13:35:21 -0500

    Initial commit

---
+++ ledBlink.ino
@@ -0,0 +1,19 @@
+#include <Adafruit_NeoPixel.h>
+Adafruit_NeoPixel neopixel = Adafruit_NeoPixel(1, 32, NEO_RGB);
+
+void setup() {
+  // put your setup code here, to run once:
+  neopixel.begin();
+  neopixel.clear();
+  neopixel.show();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  neopixel.setPixelColor(0, random(255), random(255), random(255));
+  neopixel.show();
+  delay(1000);
+  neopixel.setPixelColor(0, 0, 0, 0);
+  neopixel.show();
+  delay(1000);
+}
